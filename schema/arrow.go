dd47dce31b52f1572366cb56ea33e587d4243f66
n.lyapin <n.lyapin@postgrespro.ru>
2025-04-24 11:28:13 +0300

    use arrow decimal type

--- schema/arrow.go
+++ schema/arrow.go
@@ -90,9 +90,11 @@ func ConvertArrowToParquetSchema(schema *arrow.Schema) ([]string, error) {
 			metaData[k] = fmt.Sprintf(convertedMetaDataTemplate, v.Name,
 				parquet.Type_INT64, convertedType)
 		case "decimal":
-			const decimalMetaDataTemplate = "name=%s,type=FIXED_LEN_BYTE_ARRAY, convertedtype=DECIMAL, scale=9, precision=38, length=16"
+			const decimalMetaDataTemplate = "name=%s,type=FIXED_LEN_BYTE_ARRAY, convertedtype=DECIMAL, scale=%d, precision=%d, length=%d"
 
-			metaData[k] = fmt.Sprintf(decimalMetaDataTemplate, v.Name)
+			arrowDecimal := fieldType.(*arrow.Decimal128Type) // 'ok' check is not needed, can only be arrow.Decimal128Type
+
+			metaData[k] = fmt.Sprintf(decimalMetaDataTemplate, v.Name, arrowDecimal.Scale, arrowDecimal.Precision, arrowDecimal.BitWidth())
 		default:
 			return nil,
 				fmt.Errorf("Unsupported arrow format: %s", fieldType.Name())
